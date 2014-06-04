4555221f0a5895d2494cd8439b949afb31d5a924
Arlabunakty <nano10top>
2014-06-04 19:09:29 +0200

    Added tagsinput and some minor changes

--- js/main.js
+++ js/main.js
@@ -7,4 +7,17 @@ $(document).ready(function () {
         format: "dd/mm/yyyy"
     });
 
+    $('body').on('click', '[data-toggle="collapse"]', function() {
+        console.log(this);
+        var _self = $(this);
+        var _content = $(_self.attr('data-target'));
+        var _originalText = _self.attr('data-text');
+
+        if ( _content.hasClass('in') ) {
+            _self.text(_self.attr('data-textexp'));
+        } else {
+            _self.text(_self.attr('data-text'));
+        }
+    });
+
 });
